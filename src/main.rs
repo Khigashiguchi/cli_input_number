41ba32304b07cf4c7b7455a2881cbf986cdd93ec
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-09 14:45:38 +0900

    Implement input (but there are warnings)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Insert your first number: ");
+    io::stdout().flush();
+
+    let mut a = String::new();
+    io::stdin().read_line(&mut a);
+    let mut a: i32 = a.trim().parse().unwrap();
+
+    print!("Insert your second number: ");
+    io::stdout().flush();
+
+    let mut b = String::new();
+    io::stdin().read_line(&mut b);
+    let mut b: i32 = b.trim().parse().unwrap();
 }
